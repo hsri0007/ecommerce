afddfff4cff5eb8b8fb81baa262dfa237dc2d7d3
srinivas kameron <hsri0007@gamil.com>
2020-07-18 21:42:06 +0530

    add styling to delete

--- client/src/components/checkout/checkoutitem.component.jsx
+++ client/src/components/checkout/checkoutitem.component.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import "./checkout.style.scss";
 import { clearItem, removeItem, addItem } from "../../redux/cart/cart.action";
 import { connect } from "react-redux";
+import Swal from "sweetalert2";
 
 const Check = ({ cartItem, clearItem, removeItem, addItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
+  const deleteItem = (cartItem) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.value) {
+        clearItem(cartItem);
+      }
+    });
+  };
   return (
     <div>
       <div className="check-header">
@@ -34,7 +50,7 @@ const Check = ({ cartItem, clearItem, removeItem, addItem }) => {
           {" "}
           <h3>${price}</h3>
         </div>
-        <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        <div className="remove-button" onClick={() => deleteItem(cartItem)}>
           <h3>x</h3>
         </div>
       </div>
